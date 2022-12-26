36baf4a54b14d713b0386ebbb3bb9bea02f2c72f
ledhcg <dinhcuong.firewin99@gmail.com>
2022-12-26 18:00:28 +0300

    Up

---
+++ README.md
@@ -0,0 +1,33 @@
+# URL shortener
+
+To generate short URL which is to use in warnings and so on.
+
+## Usage
+
+Git clone this repo to your local, and run following command:
+
+```bash
+> npm run generate https://www.alipay.com
+
+https://www.alipay.com ==shorten=> https://ledinhcuong.com/987c5dc
+
+URLs had been shortened, please commit and push it to GitHub!
+```
+
+To generate a link with a specific name, specify the name after the link:
+
+```bash
+> npm run generate https://www.example.com Example Name
+
+https://www.example.com ==shorten=> https://ledinhcuong.com/example-name
+
+URLs had been shortened, please commit and push it to GitHub!
+```
+
+## Build Your Own URL Shortener
+
+You can fork this repo, and edit CNAME file with your own domain name.
+
+## License
+
+MIT
---
+++ scripts/generate.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+require('../url.map.js');
+const currentURLMap = Object.assign({}, global.URLMap);
+
+function generateUrl(target) {
+  const md5 = crypto.createHash('sha256').update(target).digest('base64');
+  return md5.slice(0, 7);
+}
+
+function slugify(shortName) {
+  return shortName.replace(/[^A-Za-z-]/g,'-').toLowerCase();
+}
+
+function addUrl(target, shortName) {
+  if(!shortName) {
+    shortName = generateUrl(target);
+  }
+  currentURLMap[`/${shortName}`] = target;
+  console.log(`${target} ==shorten=> https://ledinhcuong.com/${shortName}`);
+}
+
+const URLs = process.argv.slice(2);
+if(URLs.length > 1 && !URLs[1].startsWith('https')) {
+  addUrl(URLs[0], slugify(URLs.slice(1).join(' ')));
+} else {
+  URLs.forEach(url => addUrl(url));
+}
+
+fs.writeFileSync(
+  path.join(process.cwd(), './url.map.js'),
+  `(typeof window !== 'undefined' ? window : global).URLMap = ${JSON.stringify(currentURLMap, null, 2)};`
+);
+
+console.log('\nURLs had been shortened, please commit and push it to GitHub!');
