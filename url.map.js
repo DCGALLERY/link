14bc0578fc6d2281514905de110c0666a0cdaacb
ledhcg <dinhcuong.firewin99@gmail.com>
2022-12-26 18:45:14 +0300

    Up

--- url.map.js
+++ url.map.js
@@ -1,3 +1,5 @@
 (typeof window !== 'undefined' ? window : global).URLMap = {
+  "/undefined": "https://ledinhcuong.com/",
+  "/": "https://ledinhcuong.com",
   "/google": "https://www.google.com"
 };
