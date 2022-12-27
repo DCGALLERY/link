db9e18f8091ee45bb3a04bc39aeb99550f827157
ledhcg <dinhcuong.firewin99@gmail.com>
2022-12-28 01:55:24 +0300

    Up

--- url.map.js
+++ url.map.js
@@ -1,5 +1,6 @@
 (typeof window !== 'undefined' ? window : global).URLMap = {
   "/undefined": "https://ledinhcuong.com/",
   "/": "https://ledinhcuong.com",
-  "/google": "https://www.google.com"
+  "/google": "https://www.google.com",
+  "/kur": "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw"
 };
