d4b920d471b175ca5adf90e38bf7f79176dedbc9
ledhcg <dinhcuong.firewin99@gmail.com>
2023-01-04 04:05:22 +0300

    Up

--- url.map.js
+++ url.map.js
@@ -5,5 +5,6 @@
   "/kur": "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw",
   "/phuong-hoa-wedding-slideshow": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
   "/phuong-hoa-wedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/phuonghoawedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1"
+  "/phuonghoawedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/9AHEYI5": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1"
 };
