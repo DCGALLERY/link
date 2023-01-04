56d4bf3fd19119dc24025e8e260d70d6bab11e00
ledhcg <dinhcuong.firewin99@gmail.com>
2023-01-04 03:55:35 +0300

    Up

--- url.map.js
+++ url.map.js
@@ -2,5 +2,6 @@
   "/undefined": "https://ledinhcuong.com/",
   "/": "https://ledinhcuong.com",
   "/google": "https://www.google.com",
-  "/kur": "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw"
+  "/kur": "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw",
+  "/phuong-hoa-wedding-slideshow": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1"
 };
