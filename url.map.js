fe75ec6d2e113896e429a3299d34eb2bdc1225c9
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-04 12:42:11 +0300

    Update url.map.js

--- url.map.js
+++ url.map.js
@@ -1,11 +1,26 @@
-(typeof window !== 'undefined' ? window : global).URLMap = {
+(typeof window !== "undefined" ? window : global).URLMap = {
   "/undefined": "https://ledinhcuong.com/",
   "/": "https://ledinhcuong.com",
   "/google": "https://www.google.com",
-  "/kur": "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw",
-  "/phuong-hoa-wedding-slideshow": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/phuong-hoa-wedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/phuonghoawedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/9AHEYI5": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/E1lxK+p": "https://followers.mireavn.com"
+  "/kur":
+    "https://ledinhcuong-my.sharepoint.com/:w:/p/mail/Ef--fb7-gixPpPzRpRYdrQABgOmWpI5S4WLdC3AP15tAhw",
+  "/phuong-hoa-wedding-slideshow":
+    "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/phuong-hoa-wedding":
+    "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/phuonghoawedding":
+    "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/9AHEYI5":
+    "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/E1lxK+p": "https://flowers.mireavn.com/E1lxK+p",
+  "/ZRIC+AZ": "https://flowers.mireavn.com/ZRIC+AZ",
+  "/S4pBfDM": "https://flowers.mireavn.com/S4pBfDM",
+  "/WwDlBeR": "https://flowers.mireavn.com/WwDlBeR",
+  "/a2QF9PE": "https://flowers.mireavn.com/a2QF9PE",
+  "/5pm24KA": "https://flowers.mireavn.com/5pm24KA",
+  "/c3uZoNQ": "https://flowers.mireavn.com/c3uZoNQ",
+  "/+gnBNn5": "https://flowers.mireavn.com/+gnBNn5",
+  "/oi2WF5F": "https://flowers.mireavn.com/oi2WF5F",
+  "/RY22EFW": "https://flowers.mireavn.com/RY22EFW",
+  "/I+tZNir": "https://flowers.mireavn.com/I+tZNir",
 };
