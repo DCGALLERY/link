d35e94435bd9b517be5b3e472c8a3730b0de5027
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-04 12:18:31 +0300

    Update url.map.js

--- url.map.js
+++ url.map.js
@@ -6,5 +6,6 @@
   "/phuong-hoa-wedding-slideshow": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
   "/phuong-hoa-wedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
   "/phuonghoawedding": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
-  "/9AHEYI5": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1"
+  "/9AHEYI5": "https://ledinhcuong-my.sharepoint.com/:v:/p/mail/Edtv0YyL_AZMu0JuzAJgZbsB4XMYDvLMBm9wuHSQ2ODIHg?e=UDbYX1",
+  "/E1lxK+p": "https://followers.mireavn.com"
 };
